e0a80b4ef10ac12a243285d5eda1270e04c32370
Scott Came <scottcame10@gmail.com>
2019-11-04 17:03:15 -0800

    Added reactivity example for ts

--- ui/src/components/DatasetsView.svelte
+++ ui/src/components/DatasetsView.svelte
@@ -1,1 +1,10 @@
+<script lang="typescript">
+
+  // added this as an example of reactive expressions using ts
+  // let count = 0;
+  // let doubled;
+  // $: doubled = count * 2;
+
+</script>
+
 <div>This is the datasets view.</div>
