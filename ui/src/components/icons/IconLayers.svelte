412a0b0b92de4dbb47d1404f6cd75a484206ca37
Scott Came <scottcame10@gmail.com>
2019-11-09 10:06:37 -0800

    Factor tree container icons

---
+++ ui/src/components/icons/IconLayers.svelte
@@ -0,0 +1,5 @@
+<svg class="h-6 w-6 stroke-current" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" stroke-width="1.5">
+  <g fill="none">
+    <path d="M10,1 L20,7 L10,13 L0,7 L10,1 Z M16.6666667,11 L20,13 L10,19 L0,13 L3.33333333,11 L10,15 L16.6666667,11 Z"></path>
+  </g>
+</svg>
