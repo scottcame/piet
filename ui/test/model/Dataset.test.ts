5a85f856452dde029d9e8a43201cfdb11cbb50f0
Scott Came <scottcame10@gmail.com>
2019-11-10 08:57:40 -0800

    Shore up UI model tests

--- ui/test/model/Dataset.test.ts
+++ ui/test/model/Dataset.test.ts
@@ -2,6 +2,7 @@
 
 import { Dataset } from '../../src/js/model/Dataset';
 import * as metadata from '../_data/test-metadata.json';
+import { TreeModelContainerNode, TreeModelLeafNode } from '../../src/js/ui/model/Tree';
 
 //import { Logger } from '../../src/js/util/Logger';
 //let logger = Logger.getInstance();
@@ -10,7 +11,7 @@ const datasetId = "http://localhost:58080/mondrian-rest/getMetadata?connectionNa
 const datasets = Dataset.loadFromMetadata(metadata, datasetId);
 
 test('dataset', () => {
-  expect(datasets.length).toBe(2);
+  expect(datasets).toHaveLength(2);
   datasets.forEach((dataset: Dataset, idx: number) => {
     expect(dataset.schemaName).toBe(metadata.name);
     expect(dataset.id).toBe(datasetId);
@@ -55,4 +56,33 @@ test('hierarchies', () => {
       });
     });
   });
+});
+
+test('tree model', () => {
+  const ds = datasets[0];
+  const rootTreeModelNode = ds.rootTreeModelNode;
+  expect(rootTreeModelNode instanceof TreeModelContainerNode).toBeTruthy();
+  expect(rootTreeModelNode.label).toBe("Dataset: " + ds.description + " [" + ds.schemaName + "]");
+  expect(rootTreeModelNode.type).toBe('dataset');
+  expect(rootTreeModelNode.hasChildren()).toBeTruthy();
+  let children = rootTreeModelNode.children;
+  expect(children).toHaveLength(2);
+  let child = children[0];
+  expect(child instanceof TreeModelContainerNode).toBeTruthy();
+  let containerChild = child as TreeModelContainerNode;
+  expect(containerChild.label).toBe("Measures");
+  children = containerChild.children;
+  expect(children).toHaveLength(5);
+  child = children[0];
+  expect(child instanceof TreeModelLeafNode).toBeTruthy();
+  expect(child.label).toBe("F1_M1");
+  child = rootTreeModelNode.children[1];
+  expect(child instanceof TreeModelContainerNode).toBeTruthy();
+  containerChild = child as TreeModelContainerNode;
+  expect(containerChild.label).toBe("Dimensions");
+  children = containerChild.children;
+  expect(children).toHaveLength(2);
+  child = children[0];
+  expect(child instanceof TreeModelLeafNode).toBeTruthy();
+  expect(child.label).toBe("D1");
 });
--- ui/test/model/Model.test.ts
+++ ui/test/model/Model.test.ts
@@ -1,6 +1,25 @@
 import { Model } from '../../src/js/model/Model';
+import * as metadata from '../_data/test-metadata.json';
+import { Dataset } from '../../src/js/model/Dataset';
+import { Analysis } from '../../src/js/model/Analysis';
+import { DropdownItem } from '../../src/js/ui/model/Dropdown';
+
+const m = new Model();
+const datasetId = "http://localhost:58080/mondrian-rest/getMetadata?connectionName=test";
+const datasets = Dataset.loadFromMetadata(metadata, datasetId);
 
 test('model initialization', () => {
-  const m = new Model();
   expect(m.analyses.length).toBe(0);
+});
+
+test('analyses select model', () => {
+  m.datasets = datasets;
+  m.analyses = datasets.map((dataset, idx) => {
+    return new Analysis(dataset, "Analysis " + idx);
+  });
+  const dropdownModel = m.analysesSelectModel;
+  expect(dropdownModel.items).toHaveLength(m.analyses.length);
+  dropdownModel.items.forEach((val: DropdownItem, idx: number) => {
+    expect(val.label).toBe(m.analyses[idx].name);
+  });
 });
