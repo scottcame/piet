c1e88718add5f8d7e3e31453e5fd4fe410eaf4cd
Scott Came <scottcame10@gmail.com>
2019-11-10 08:20:24 -0800

    add/remove listener on open/close of dropdown

--- ui/src/components/Dropdown.svelte
+++ ui/src/components/Dropdown.svelte
@@ -7,12 +7,6 @@
   let containerDiv;
   const dispatch = createEventDispatcher();
 
-  document.addEventListener("click", e => {
-    if (!containerDiv.contains(e.target)) {
-      open = false;
-    }
-  });
-
   function selectItem(idx) {
     let old = model.selectedIndex;
     model.selectedIndex = idx;
@@ -23,10 +17,25 @@
     });
   }
 
+  let outsideClickListener = e => {
+    if (!containerDiv.contains(e.target)) {
+      open = false;
+    }
+  }
+
+  function toggleOpen(event) {
+    open = !open;
+    if (open) {
+      document.addEventListener("click", outsideClickListener);
+    } else {
+      document.addEventListener("click", outsideClickListener);
+    }
+  }
+
 </script>
 
 <div class="relative ml-2 mb-3" bind:this="{containerDiv}">
-  <button type="button" class="w-full block bg-gray-300 font-semibold rounded py-2 focus:outline-none border-gray-800" on:click="{e => open = !open}">
+  <button type="button" class="w-full block bg-gray-300 font-semibold rounded py-2 focus:outline-none border-gray-800" on:click={toggleOpen}>
     {model.selectedItem === null ? "Choose an analysis..." : model.selectedItem.label}
   </button>
   {#if open}
