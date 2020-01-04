de6eb5dd43eded79d9a7f5f3d89b364282da595c
Scott Came <scottcame10@gmail.com>
2020-01-04 09:10:19 -0800

    Select first analysis in dropdown upon view init

--- ui/src/components/AnalysesView.svelte
+++ ui/src/components/AnalysesView.svelte
@@ -92,7 +92,7 @@
 <div class="mt-2 p-2 h-full bg-gray-100 flex flex-inline {viewProperties.analysesInWorkspace ? '' : 'hidden'}">
   <div class="w-1/4 h-full select-none pt-2 pr-2 border-2 overflow-y-auto">
     <div class="flex flex-inline items-center justify-between mb-2">
-      <Dropdown dropdownModel={controller.analysesDropdownModel} showCaret="true"/>
+      <Dropdown dropdownModel={controller.analysesDropdownModel} showCaret="true" initialSelectionIndex="0"/>
     </div>
     <TreeContainerNode treeModelNode={viewProperties.datasetRootTreeModelNode} collapsable={false} on:nodeEvent={handleDatasetTreeNodeEvent} currentAnalysis={viewProperties.currentAnalysis}/>
   </div>
