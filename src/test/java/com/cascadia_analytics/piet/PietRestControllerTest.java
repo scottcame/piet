9897e6313a906d4d7795d2c450f09af58e1d063a
Scott Came <scottcame10@gmail.com>
2021-06-13 12:49:31 -0700

    jupiter tests

--- src/test/java/com/cascadia_analytics/piet/PietRestControllerTest.java
+++ src/test/java/com/cascadia_analytics/piet/PietRestControllerTest.java
@@ -15,8 +15,8 @@
 
 package com.cascadia_analytics.piet;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.apache.commons.logging.Log;
--- src/test/java/com/cascadia_analytics/piet/repository/AnalysisRepositoryTest.java
+++ src/test/java/com/cascadia_analytics/piet/repository/AnalysisRepositoryTest.java
@@ -15,9 +15,9 @@
 
 package com.cascadia_analytics.piet.repository;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
